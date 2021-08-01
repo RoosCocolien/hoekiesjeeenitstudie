1ec724a3de0ba649e5f38aa34beb5cfd5986a2ec
Roos Cocolien <r.steigenga@gmail.com>
2021-08-01 22:46:36 +0200

    started with styling, created a build, put it online cocolien.nl

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
 	}
 
 	body {
+		margin: 10px;
 		text-align: center;
 		font-family: 'IBM Plex Mono';
 		background: rgb(255, 255, 255);
@@ -16,43 +17,51 @@ export const GlobalStyle = createGlobalStyle`
 	h1, h2 {
 		font-family: 'Tourney', mono space;
 	}
-
-	* {
-		box-sizing: border-box;
-		font-family: 'IBM Plex Mono', mono space;
-	}
-
 `;
 
 export const ResultWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
 
-	> p {
-		color: #fff;
-		font-size: 2rem;
-		margin: 10px;
+	margin: 0px;
+	padding: 10px;
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		align-items: center;
+	}
+
+	@media(max-width: 600px) {
+		.row {
+			display: block;
+		}
+		h1, h2 {
+			font-size: 22px;
+		}
 	}
 
 	.ResultBootcamp {
+		padding: 10px;
 		background: rgb(0,210,162);
 		background: linear-gradient(180deg, rgba(0,210,162,1) 0%, rgba(255,126,255,1) 23%, rgba(255,126,255,1) 53%, rgba(167,255,199,1) 100%);
 	}
 	.ResultOnline {
+		padding: 10px;
 		background: rgb(167,255,199);
 		background: linear-gradient(180deg, rgba(167,255,199,1) 0%, rgba(192,231,157,1) 62%, rgba(194,65,194,1) 100%);
 	}
 	.ResultTrad {
+		padding: 10px;
 		background: rgb(194,65,194);
 		background: linear-gradient(180deg, rgba(194,65,194,1) 0%, rgba(194,65,194,1) 48%, rgba(187,114,195,1) 64%, rgba(251,176,59,1) 100%);
 	}
 	.ResultTraineeship {
-
+		padding: 10px;
 		background: rgb(251,176,59);
 		background: linear-gradient(180deg, rgba(251,176,59,1) 0%, rgba(225,197,84,1) 54%, rgba(153,255,153,1) 100%);	
 	}
 	.ResultAlternative {
+		padding: 10px;
 		background: rgb(153,255,153);
 		background: linear-gradient(180deg, rgba(153,255,153,1) 0%, rgba(153,255,153,1) 40%, rgba(0,210,162,1) 100%);
 	}
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -22,71 +22,99 @@ const Result: React.FC<Props> = ({answerResults}) => {
 	return (
 		<ResultWrapper>
 			<h1>Resultaat</h1>
-			<div className="resultIntroduction">
-				<img src={bootcamp} alt="bootcamp icon"/>
-				<img src={online} alt="zelfstudie icon"/>
-				<img src={trad} alt="traditioneel onderwijs icon"/>
-				<img src={traineeship} alt="traineeship icon"/>
-				<img src={alternative} alt="alternatief onderwijs icon"/>
-				<p>
-					{percentage.bootcamp}%  {percentage.online}%  {percentage.trad}%  {percentage.traineeship}%  {percentage.alternative}%
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-				</p>
-				<br/>
-				<br/>
-				<br/>
-			</div>
-			<div className="ResultBootcamp">
-				<img src={bootcamp} alt="bootcamp icon"/>
-				<h2>Bootcamp</h2>
-				<h3>{percentage.bootcamp}%</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-					<br/>
-					Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
-				</p>
-			</div>
-			<div className="ResultOnline">
-				<img src={online} alt="zelfstudie icon"/>
-				<h2>Zelfstudie</h2>
-				<h3>{percentage.online}%</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-					<br/>
-					Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
-				</p>
-			</div>
-			<div className="ResultTrad">
-				<img src={trad} alt="traditioneel onderwijs icon"/>
-				<h2>Traditioneel onderwijs</h2>
-				<h3>{percentage.trad}%</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-					<br/>
-					Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
-				</p>
-			</div>
-			<div className="ResultTraineeship">
-				<img src={traineeship} alt="traineeship icon"/>
-				<h2>Traineeship</h2>
-				<h3>{percentage.traineeship}%</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-					<br/>
-					Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
-				</p>
-			</div>
-			<div className="ResultAlternative">
-				<img src={alternative} alt="alternatief onderwijs icon"/>
-				<h2>Alternatief onderwijs</h2>
-				<h3>{percentage.alternative}%</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
-					<br/>
-					Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
-				</p>
+			{/* <div className="resultIntroduction"> */}
+				<div className="container">
+					<div className="row">
+						<div className="col-2 col-md-12">
+							<img src={bootcamp} alt="bootcamp icon"/>
+							<br/>
+							{percentage.bootcamp}%
+						</div>
+						<div className="col-2 col-md-12">
+							<img src={online} alt="zelfstudie icon"/>
+							<br/>
+							{percentage.online}%
+						</div>
+						<div className="col-2 col-md-12">
+							<img src={trad} alt="traditioneel onderwijs icon"/>
+							<br/>
+							{percentage.trad}%
+						</div>
+						<div className="col-2 col-md-12">
+							<img src={traineeship} alt="traineeship icon"/>
+							<br/>
+							{percentage.traineeship}%
+						</div>
+						<div className="col-2 col-md-12">
+							<img src={alternative} alt="alternatief onderwijs icon"/>
+							<br/>
+							{percentage.alternative}%
+						</div>
+					</div>
+					<div className="row justify-content-center">
+						<div className="col">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+							</p>
+						</div>
+					</div>
+				</div>
+			{/* </div> */}
+			
+			<div className="Result">
+
+				<div className="ResultBootcamp">
+					<img src={bootcamp} alt="bootcamp icon"/>
+					<h2>Bootcamp</h2>
+					<h3>{percentage.bootcamp}%</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+						<br/>
+						Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
+					</p>
+				</div>
+				<div className="ResultOnline">
+					<img src={online} alt="zelfstudie icon"/>
+					<h2>Zelfstudie</h2>
+					<h3>{percentage.online}%</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+						<br/>
+						Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
+					</p>
+				</div>
+				<div className="ResultTrad">
+					<img src={trad} alt="traditioneel onderwijs icon"/>
+					<h2>Traditioneel onderwijs</h2>
+					<h3>{percentage.trad}%</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+						<br/>
+						Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
+					</p>
+				</div>
+				<div className="ResultTraineeship">
+					<img src={traineeship} alt="traineeship icon"/>
+					<h2>Traineeship</h2>
+					<h3>{percentage.traineeship}%</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+						<br/>
+						Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
+					</p>
+				</div>
+				<div className="ResultAlternative">
+					<div className="ResultImage">
+						<img src={alternative} alt="alternatief onderwijs icon"/>
+					</div>
+					<h2>Alternatief onderwijs</h2>
+					<h3>{percentage.alternative}%</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet interdum ipsum, non malesuada mauris. Proin ut erat sit amet lorem facilisis commodo. Aliquam maximus dui vel nunc bibendum auctor. Curabitur orci mi, dignissim nec rutrum et, pharetra vitae justo. Phasellus cursus elit sollicitudin dui sagittis, id varius odio condimentum. Donec sed tellus non ex auctor rhoncus sit amet vitae sem. Suspendisse blandit nunc dui, sit amet tristique metus pulvinar et.
+						<br/>
+						Fusce nec magna nulla. Integer non risus nec orci tincidunt finibus at in dolor. Vivamus ullamcorper odio sed commodo venenatis. Morbi venenatis ornare justo quis congue. Praesent id imperdiet velit. Proin consequat tellus ipsum, fermentum lacinia dolor feugiat vitae. Vivamus sagittis quam a mauris dictum, vitae feugiat tellus vestibulum. Sed fermentum commodo velit, a tempor risus rhoncus quis. Nullam congue convallis tortor vestibulum sodales. Ut bibendum nisl at iaculis tincidunt. Quisque leo nulla, maximus non dapibus sit amet, aliquet euismod ex. Donec mi nisi, consectetur a lobortis et, lobortis vel mi. Proin tincidunt sagittis orci non dignissim. Vivamus gravida laoreet tempus. Vestibulum pulvinar nisl non nulla blandit venenatis.
+					</p>
+				</div>
 			</div>
 		</ResultWrapper>
 	)
